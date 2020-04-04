dce6436669650602e675421c521cf88074fdd772
Akash <akashmenon11699cool@hotmail.com>
2020-04-04 11:04:52 +0530

    added auth middleware

---
+++ src/middleware/auth.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/userModel');
+
+const auth = async(req, res, next) => {
+    try {
+        const token = req.header('Authorization').replace('Bearer ', '');
+        const decoded = jwt.verify(token, 'lacablood');
+        const user = await User.findOne({ _id: decoded.id, 'tokens.token': token });
+        if (!user) {
+            res.status(400).send('Authentication failed');
+        }
+        req.user = user;
+        req.token = token;
+        next();
+    } catch (error) {
+        res.status(400).send('Authentication failed');
+    }
+};
+
+module.exports = auth;
